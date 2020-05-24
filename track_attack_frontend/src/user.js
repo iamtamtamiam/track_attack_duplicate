65a81b2f9a07e3bff8bbf166f81144fb905db344
iamtamtamiam <vutam_a@hotmail.com>
2020-05-24 14:23:03 -0500

    create user.js file

---
+++ track_attack_frontend/src/user.js
@@ -0,0 +1,29 @@
+class User {
+
+    constructor(userJson){
+        this.id = userJson["user"]["id"]
+        this.username = userJson["user"]["username"] 
+    } //call new user when logging in/signup 
+
+
+    //rendering
+
+    renderUserWelcomeMessage(){
+        //hides the failure alert
+        //hitdes the login form
+        //displays welcome
+        console.log(this)
+            //if logged in, hide the failure alert
+            //need to add welcome user and hide signup/login form
+            alertButton.setAttribute("class", "hidden")
+            alertButton.innerText = "" 
+            document.getElementById("login-form").style.display="none"
+            let weclcomeMessage = document.getElementById("welcome-user")
+            //weclcomeMessage.innerText = `Welcome ${json["user"]["username"]}!`
+            weclcomeMessage.innerText = `Welcome ${this.username}!`
+
+    }
+
+
+
+}
