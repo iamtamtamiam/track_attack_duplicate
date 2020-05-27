07b96d339d828ed4ac9d6e0de17ea18d2382fba5
iamtamtamiam <vutam_a@hotmail.com>
2020-05-27 12:08:25 -0500

    add games#index

--- track_attack_backend/app/controllers/games_controller.rb
+++ track_attack_backend/app/controllers/games_controller.rb
@@ -1,7 +1,28 @@
 class GamesController < ApplicationController
 
     def index
+        games = Game.all
+        render json: games
+    end 
 
+
+    def new
+    end
+
+    def create
+        game = Game.new(game_params)
+        if game.save
+            render json: game
+        else
+            render game.errors.as_json(full_messages: true)
+        end
+    end
+
+
+    private
+
+    def game_params
+        params.require(:game).permit(:name, :user_id)
     end 
 
 
