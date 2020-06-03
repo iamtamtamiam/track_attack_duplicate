6cecced3c5dc0500922b9d71398b272bde30932b
iamtamtamiam <vutam_a@hotmail.com>
2020-06-02 23:11:52 -0500

    add selections route and controller

---
+++ track_attack_backend/app/controllers/selections_controller.rb
@@ -0,0 +1,43 @@
+class SelectionsController < ApplicationController
+
+    def index
+        selections = Selection.all
+        
+        
+
+        render json: selections
+    end 
+
+
+    def new
+    end
+
+    def create
+        selection = Selection.new(selection_params)
+        #byebug
+        if selection.save
+            render json: selection
+        else
+            render json:{
+                status: 401,
+                main: selection.errors.as_json(full_messages: true)
+            }
+        end
+    end
+
+    def show
+
+    end 
+
+
+    private
+
+    def selection_params
+        params.require(:selection).permit(:character_id)
+    end 
+
+    def current_user
+        @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
+      end 
+
+end 
